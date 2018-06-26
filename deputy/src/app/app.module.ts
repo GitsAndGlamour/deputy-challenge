9170d572ca7726bb661fc503dbf567675a345e3a
Omie Walls <omierwalls@gmail.com>
2018-06-25 23:37:42 -0400

    HTML/CSS Bootstrap Form

--- deputy/src/app/app.module.ts
+++ deputy/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { MaterialModule } from './material/material.module';
 import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule
